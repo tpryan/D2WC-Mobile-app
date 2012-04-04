f3293735eb7780832920892692ff4c8a7c4be969
Terrence Ryan <terry@terrenceryan.com>
2012-04-03 21:33:08 -0400

    Adding page transitions with no jQuery.

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var readyFunc = [];
 
 
 ready(function () {
-	//replaceLinks();
+	replaceLinks();
 });
 
 
@@ -40,18 +40,144 @@ function replaceLinks(){
 function replacePage(){
 	event.preventDefault();
 	var href= this.href;
-	var stateObj = { foo: "bar" };
-	alert(href);
+	
+	
 	
 	var ajax = new XMLHttpRequest();
 	ajax.open("GET",href,true);
 	ajax.send();
-	
+
+
 	ajax.onreadystatechange=function(){
 		if(ajax.readyState==4 && (ajax.status==200)){
-			document.querySelector('html').innerHTML = ajax.responseText;
-			history.pushState(stateObj, "page 2", href);
+			var body = document.querySelector('body');
+
+			var bodyContent =  grabBettwenTags(ajax.responseText, "body");
+			var bodyID = grabBodyID(ajax.responseText);
+			var bodyClasses = grabBodyClass(ajax.responseText);
+
+			
+
+			function returnToCenter(event){
+				var body = document.querySelector('body');
+				body.removeEventListener( 'webkitTransitionEnd', returnToCenter, false);
+				body.innerHTML = bodyContent;
+				body.id = bodyID;
+				body.className = bodyClasses;
+				history.pushState(null, null, href);
+
+				window.addEventListener("popstate", 
+					function(e) {
+
+				   		var ajaxBack = new XMLHttpRequest();
+						ajaxBack.open("GET",location.pathname,true);
+						ajaxBack.send();
+
+
+						ajaxBack.onreadystatechange=function(){
+							var bodyBack = document.querySelector('body');
+							var bodyBackContent =  grabBettwenTags(ajaxBack.responseText, "body");
+							var bodyBackID = grabBodyID(ajaxBack.responseText);
+							var bodyBackClasses = grabBodyClass(ajaxBack.responseText);
+
+							function backToCenter(event){
+								var body = document.querySelector('body');
+								body.removeEventListener( 'webkitTransitionEnd', backToCenter, false);
+								body.innerHTML = bodyBackContent;
+								body.id = bodyBackID;
+								body.className = bodyBackClasses;
+
+								body.style.opacity = 1;
+								body.style.left = 0;
+								replaceLinks();
+							}
+
+							function moveToLeft(event){
+								var body = document.querySelector('body');
+								body.removeEventListener( 'webkitTransitionEnd', moveToLeft, false);
+								body.addEventListener( 'webkitTransitionEnd', backToCenter, false);
+								body.style.opacity = 0;
+								body.style.left = "-100%"
+							}
+
+							body.addEventListener( 'webkitTransitionEnd', moveToLeft, false);
+							body.style.left = "100%";
+
+						}
+
+
+					}
+				);
+
+
+				body.style.opacity = 1;
+				body.style.left = 0;
+				replaceLinks();
+			}
+
+			function moveToRight(event){
+				var body = document.querySelector('body');
+				body.removeEventListener( 'webkitTransitionEnd', moveToRight, false);
+				body.addEventListener( 'webkitTransitionEnd', returnToCenter, false);
+				body.style.opacity = 0;
+				body.style.left = "100%"
+			}
+
+
+			body.addEventListener( 'webkitTransitionEnd', moveToRight, false);
+
+			body.style.left = "-100%";
+
+
 		}
-	} 
+	}		
+
+
 	
 }
+
+function grabBettwenTags(html, tag){
+	var tag = typeof tag !== 'undefined' ? tag : "p";
+	var tagStart = html.indexOf("<" + tag);
+	var start = html.indexOf(">", tagStart) + 1;
+	var end = html.indexOf("</" + tag, start );
+	return html.slice(start, end);
+}
+
+function grabBodyID(html){
+	var result = "";
+	var tagStart = html.indexOf("<body");
+	var start = html.indexOf('id="', tagStart) + 4;
+	var end = html.indexOf('"', start );
+
+	if (start - tagStart < 20){
+		if (end > start){
+			result = html.slice(start, end);
+		}
+	}	
+
+	console.log("start: 	'" + start + "'");
+	console.log("end: 	'" + end + "'");
+	console.log("result: 	'" + result + "'");
+	
+
+	return result.trim();
+}
+
+function grabBodyClass(html){
+	var list = "";
+	var result = "";
+	var tagStart = html.indexOf("<body");
+	var start = html.indexOf('class="', tagStart) + 7;
+	var end = html.indexOf('"', start );
+	
+	if (start - tagStart < 20){
+		if (end > start){
+			list = html.slice(start, end);
+		}
+	}
+
+	
+	return list;
+}
+
